6581004bfe95b559b1865f1e3c35aaee90a467ee
antony <tehanovanton@gmail.com>
2022-11-07 22:00:04 +0300

    task3 work

---
+++ src/main/java/filters/F3.java
@@ -0,0 +1,30 @@
+package filters;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class F3 implements Filter {
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    System.out.println("F3:init");
+  }
+
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    String paramValue = servletRequest.getParameter("value3");
+    
+    if (paramValue != null && servletRequest.getParameter("value3").equals("3")) {
+      System.out.println("F3:doFilter");
+      filterChain.doFilter(servletRequest, servletResponse);
+    } else {
+      PrintWriter out = servletResponse.getWriter();
+      out.println("F3, value1!=3");
+    }
+  }
+
+  @Override
+  public void destroy() {
+    System.out.println("F3:destroy");
+  }
+}
