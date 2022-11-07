fca5b5cb257dfc40f33ed43a580f12be9d59e706
antony <tehanovanton@gmail.com>
2022-11-07 21:10:34 +0300

    lab7 done

--- src/main/java/Ccc.java
+++ src/main/java/Ccc.java
@@ -5,61 +5,78 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import models.CBean;
 
-public class Ccc extends HttpServlet {
+public class Ccc extends HttpServlet{
     @Override
     public void init() throws ServletException {
-        super.init();
-        getServletContext().setAttribute("atrCBean", new CBean());
-        // setValues("1", "2", "3");
+      super.init();
     }
-
+  
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/ccc.jsp");
-        requestDispatcher.forward(req, resp);
+      RequestDispatcher requestDispatcher = req.getRequestDispatcher("/ccc.jsp");
+      requestDispatcher.forward(req, resp);
     }
-
+  
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        getResult(req, resp);
+      getResult(req, resp);
     }
-
+  
     private void getResult(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String value1 = req.getParameter("value1");
-        String value2 = req.getParameter("value2");
-        String value3 = req.getParameter("value3");
-        String cbean = req.getParameter("cbean");
+      HttpSession session = req.getSession();
+      String sessionId = session.getId();
 
-        if (cbean != null && cbean.equals("new")) {
-            System.out.println("new CBean");
-            getServletContext().setAttribute("atrCBean", new CBean());
-        }
-        else {
-            System.out.println("old CBean");
-        }
+      CBean cbeanSession = (CBean) session.getAttribute(sessionId);
+      CBean objForSession = new CBean();
+      CBean objForRequest = new CBean();
 
-        setValues(value1, value2, value3);
-
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/ccc.jsp");
-        requestDispatcher.forward(req, resp);
+      String value1 = req.getParameter("value1");
+      String value2 = req.getParameter("value2");
+      String value3 = req.getParameter("value3");
+      
+      String cbean = req.getParameter("cbean");
+  
+      if (cbeanSession != null) {
+        objForSession = cbeanSession;
+      } else {
+        session.setAttribute(sessionId, objForSession);
+        session.setMaxInactiveInterval(1000);
+      }
+  
+      if (cbean != null && cbean.equals("new")) {
+        req.removeAttribute("atrCBean");
+        req.setAttribute("atrCBean", new CBean());
+        session.removeAttribute(sessionId);
+        session.setAttribute(sessionId, new CBean());
+      }
+  
+      setValues(objForSession, objForRequest, value1, value2, value3);
+  
+      session.setAttribute(sessionId, objForSession);
+      req.setAttribute("atrCBean", objForRequest);
+  
+      RequestDispatcher requestDispatcher = req.getRequestDispatcher("/ccc.jsp");
+      requestDispatcher.forward(req, resp);
     }
-
-    private void setValues(String value1, String value2, String value3) {
-        CBean ob = (CBean) getServletContext().getAttribute("atrCBean");
-
-        if (value1 != "") {
-            ob.setValue1(value1);
-        }
-
-        if (value2 != "") {
-            ob.setValue2(value2);
-        }
-
-        if (value3 != "") {
-            ob.setValue3(value3);
-        }
+  
+    private void setValues(CBean objForSession, CBean objForRequest, String value1, String value2, String value3) {
+      if (value1 != "") {
+        objForSession.setValue1(value1);
+        objForRequest.setValue1(value1);
+      }
+  
+      if (value2 != "") {
+        objForSession.setValue2(value2);
+        objForRequest.setValue2(value2);
+      }
+  
+      if (value3 != "") {
+        objForSession.setValue3(value3);
+        objForRequest.setValue3(value3);
+      }
     }
 }
--- src/main/webapp/ccc.jsp
+++ src/main/webapp/ccc.jsp
@@ -1,14 +1,44 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ page import="javax.servlet.ServletContext" %>
 <%@ page import="models.CBean" %>
+<%@ page session="true" %>
+
 <html>
     <head>
-        <title>lab6</title>
+        <title>lab7</title>
     </head>
+
     <body>
-        <% CBean ob = (CBean) pageContext.getServletContext().getAttribute("atrCBean"); %>
-        <%=ob.getValue1()%>
-        <%=ob.getValue2()%>
-        <%=ob.getValue3()%>
+        <%
+            CBean reqObj = (CBean) request.getAttribute("atrCBean");
+            CBean sessObj = (CBean) session.getAttribute(session.getId());
+        %>
+
+        <% if (reqObj != null) { %>
+            <p>
+                From request attribute
+                <%=reqObj.getValue1()%>
+                <%=reqObj.getValue2()%>
+                <%=reqObj.getValue3()%>
+            </p>
+        <%} else {%>
+            EMPTY REQUEST
+        <% } %>        
+        <hr/>
+        <br/>
+
+        <%=session.getId()%>
+        <hr/>
+        <br/>
+
+        <% if (sessObj != null) { %>
+            <p>
+                From seession attribute:
+                <%=sessObj.getValue1()%>
+                <%=sessObj.getValue2()%>
+                <%=sessObj.getValue3()%>
+            </p>
+        <%} else {%>
+            EMPTY REQUEST
+        <% } %>                
     </body>
 </html>
